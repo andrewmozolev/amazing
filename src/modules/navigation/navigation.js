2d64cd05828fc0a9b68a32d95b9fbadbcdc03385
mozolev <avmozolev@yandex.ru>
2016-10-26 17:19:25 +0300

    Update 2016-10-26T14:19:21.352Z

--- src/modules/navigation/navigation.js
+++
@@ -1,7 +0,0 @@
-(function(){
-  var btn = document.querySelector('#btn-menu');
-  var menu = document.querySelector('#menu');
-  btn.addEventListener('click', function(evt) {
-    menu.classList.toggle('js-show');
-  });
-}());
